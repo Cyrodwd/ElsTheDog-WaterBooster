846d72c9ae9364fc743f216be00c32aaa7c26be9
Cyrodwd <rigbyr1gb1p03egaogao@gmail.com>
2025-01-10 16:02:47 -0800

    Update to parin 0.0.33

--- source/scenes/menuscene.d
+++ source/scenes/menuscene.d
@@ -6,33 +6,26 @@ import managers.scene;
 import managers.text;
 
 class MenuScene : IScene {
-    private Vec2 mButtonOffset;
-    private IStr mLabel;
+    private Vec2 buttonSize;
     private UiOptions mOptions;
 
     public override void onStart() {
-        mButtonOffset = Vec2(512, 128);
-        mLabel = "Start test";
+        buttonSize = Vec2(512, 128);
         mOptions.font = FontManager.get();
     }
 
     public override void onUpdate(float dt) {
         prepareUi();
-        useUiLayout(Layout.h);
         setUiFocus(0);
         /* Ay xD*/
     }
 
     public override void onDraw() {
-        setUiStartPoint(Vec2(8, 0));
-
-        if (uiButton(mButtonOffset, mLabel, mOptions)) {
+        if (uiButton(Rect(Vec2.zero, buttonSize), "Start test", mOptions)) {
             SceneManager.get().set("PlayScene");
         }
 
-        setUiStartPoint(Vec2(8, 256));
-
-        if (uiButton(mButtonOffset, "Parin URL test", mOptions)) {
+        if (uiButton(Rect(Vec2(0, 256), buttonSize), "Parin URL test", mOptions)) {
             openUrl();
         }
     }
